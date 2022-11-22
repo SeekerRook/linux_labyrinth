995260107d261a636ae7bb460b7032f28dbb9dde
SeekerRook <xrist.vardakis@gmail.com>
2022-11-23 01:09:06 +0200

    removed hidden generators

--- labyrinth/adam/.gen.py
+++
@@ -1,8 +0,0 @@
-print("the Flag is Lower V")
-for i in range(100000):
-    print()
-print("4ll")
-for i in range(100000):
-    print()
-print("the Flag is Higher ^")
-
--- labyrinth/broken_leg/.gen.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-cout << ("Well Done! The flag is :\n 0p3r4t1ng\n");
-}
